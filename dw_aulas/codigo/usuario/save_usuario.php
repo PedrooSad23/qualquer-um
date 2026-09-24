bcb156758667d5df02dcea7c33ae77fa0fd18098
PedrooSad23 <pedroosad23@gmail.com>
2026-09-24 08:00:08 -0300

    Update save_usuario.php

--- dw_aulas/codigo/usuario/save_usuario.php
+++ dw_aulas/codigo/usuario/save_usuario.php
@@ -1,6 +1,11 @@
 <?php
 require_once "../conexao.php";
 
+if (empty($_POST['username']) OR empty($_POST['nome']) OR empty($_POST['email']) OR empty($_POST['senha'])) {
+    header("Location: form_usuario.php?erro=vazio");
+    exit;
+}
+
 $username = $_POST['username'];
 $nome = $_POST['nome'];
 $email = $_POST['email'];
